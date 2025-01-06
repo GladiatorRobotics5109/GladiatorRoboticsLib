933450837b4aa0e2fc96aa389618d78e84b0f9c3
Nathan Q <Natethegreat08q@gmail.com>
2025-01-05 20:23:19 -0500

    Update to 2025

--- lib/src/main/java/com/github/gladiatorrobotics5109/gladiatorroboticslib/advantagekitutil/loggedgyro/LoggedGyroIONavX.java
+++ lib/src/main/java/com/github/gladiatorrobotics5109/gladiatorroboticslib/advantagekitutil/loggedgyro/LoggedGyroIONavX.java
@@ -1,43 +1,44 @@
-package com.github.gladiatorrobotics5109.gladiatorroboticslib.advantagekitutil.loggedgyro;
+// Disabled until 2025 navx libs
+// package com.github.gladiatorrobotics5109.gladiatorroboticslib.advantagekitutil.loggedgyro;
 
-import com.kauailabs.navx.frc.AHRS;
+// import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.SPI;
+// import edu.wpi.first.math.geometry.Rotation2d;
+// import edu.wpi.first.wpilibj.SPI;
 
-public class LoggedGyroIONavX implements LoggedGyroIO {
-    private final AHRS m_navX;
+// public class LoggedGyroIONavX implements LoggedGyroIO {
+// private final AHRS m_navX;
 
-    private Rotation2d m_yawOffset;
-    private Rotation2d m_pitchOffset;
-    private Rotation2d m_rollOffset;
+// private Rotation2d m_yawOffset;
+// private Rotation2d m_pitchOffset;
+// private Rotation2d m_rollOffset;
 
-    public LoggedGyroIONavX() {
-        m_yawOffset = new Rotation2d();
-        m_pitchOffset = new Rotation2d();
-        m_rollOffset = new Rotation2d();
+// public LoggedGyroIONavX() {
+// m_yawOffset = new Rotation2d();
+// m_pitchOffset = new Rotation2d();
+// m_rollOffset = new Rotation2d();
 
-        m_navX = new AHRS(SPI.Port.kMXP);
-    }
+// m_navX = new AHRS(SPI.Port.kMXP);
+// }
 
-    public void updateInputs(LoggedGyroIOInputs inputs) {
-        inputs.isConnected = m_navX.isConnected();
-        inputs.isSim = false;
-        inputs.yaw = m_navX.getRotation2d().plus(m_yawOffset);
-        inputs.pitch = Rotation2d.fromDegrees(m_navX.getPitch()).plus(m_pitchOffset);
-        inputs.roll = Rotation2d.fromDegrees(m_navX.getRoll()).plus(m_rollOffset);
-    }
+// public void updateInputs(LoggedGyroIOInputs inputs) {
+// inputs.isConnected = m_navX.isConnected();
+// inputs.isSim = false;
+// inputs.yaw = m_navX.getRotation2d().plus(m_yawOffset);
+// inputs.pitch = Rotation2d.fromDegrees(m_navX.getPitch()).plus(m_pitchOffset);
+// inputs.roll = Rotation2d.fromDegrees(m_navX.getRoll()).plus(m_rollOffset);
+// }
 
-    public void setYaw(Rotation2d yaw) {
-        m_navX.setAngleAdjustment(yaw.getDegrees());
-        // m_yawOffset = yaw.minus(m_navX.getRotation2d());
-    }
+// public void setYaw(Rotation2d yaw) {
+// m_navX.setAngleAdjustment(yaw.getDegrees());
+// // m_yawOffset = yaw.minus(m_navX.getRotation2d());
+// }
 
-    public void setPitch(Rotation2d pitch) {
-        m_pitchOffset = Rotation2d.fromDegrees(pitch.getDegrees() - m_navX.getPitch());
-    }
+// public void setPitch(Rotation2d pitch) {
+// m_pitchOffset = Rotation2d.fromDegrees(pitch.getDegrees() - m_navX.getPitch());
+// }
 
-    public void setRoll(Rotation2d roll) {
-        m_rollOffset = Rotation2d.fromDegrees(roll.getDegrees() - m_navX.getRoll());
-    }
-}
+// public void setRoll(Rotation2d roll) {
+// m_rollOffset = Rotation2d.fromDegrees(roll.getDegrees() - m_navX.getRoll());
+// }
+// }
---
+++ lib/src/main/java/com/github/gladiatorrobotics5109/gladiatorroboticslib/advantagekitutil/loggedgyro/LoggedGyroIOPigeon.java
@@ -0,0 +1,58 @@
+package com.github.gladiatorrobotics5109.gladiatorroboticslib.advantagekitutil.loggedgyro;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
+public class LoggedGyroIOPigeon implements LoggedGyroIO {
+    private final Pigeon2 m_pigeon;
+
+    private final StatusSignal<Angle> m_yaw;
+    private final StatusSignal<Angle> m_pitch;
+    private final StatusSignal<Angle> m_roll;
+
+    public LoggedGyroIOPigeon(int port, String canBus) {
+        m_pigeon = new Pigeon2(port, canBus);
+
+        m_yaw = m_pigeon.getYaw();
+        m_pitch = m_pigeon.getPitch();
+        m_roll = m_pigeon.getRoll();
+    }
+
+    public LoggedGyroIOPigeon(int port) {
+        this(port, "");
+    }
+
+    public void updateInputs(LoggedGyroIOInputs inputs) {
+        BaseStatusSignal.refreshAll(
+            m_yaw,
+            m_pitch,
+            m_roll
+        );
+
+        inputs.isConnected = m_pigeon.isConnected();
+        inputs.isSim = false;
+        inputs.yaw = Rotation2d.fromRadians(m_yaw.getValue().in(Units.Radians));
+        inputs.pitch = Rotation2d.fromRadians(m_pitch.getValue().in(Units.Radians));
+        inputs.roll = Rotation2d.fromRadians(m_roll.getValue().in(Units.Radians));
+    }
+
+    @Override
+    public void setYaw(Rotation2d yaw) {
+        m_pigeon.setYaw(yaw.getMeasure());
+    }
+
+    @Override
+    public void setPitch(Rotation2d pitch) {
+        throw new UnsupportedOperationException("Unimplemented method 'setPitch'");
+    }
+
+    @Override
+    public void setRoll(Rotation2d roll) {
+        throw new UnsupportedOperationException("Unimplemented method 'setYaw'");
+    }
+}
